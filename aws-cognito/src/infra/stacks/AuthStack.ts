495068b66c731ddec59826a4e377901a4688defe
krishan nandan sharma <krishannandan09@gmail.com>
2025-09-19 07:15:20 +0530

    changes for adding admin group and creating identity pool

--- aws-cognito/src/infra/stacks/AuthStack.ts
+++ aws-cognito/src/infra/stacks/AuthStack.ts
@@ -1,15 +1,19 @@
 import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
-import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
+import { CfnIdentityPool, CfnUserPoolGroup, UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
 
 
 export class AuthStack extends Stack {
     public userPool: UserPool;
+    private identityPool: CfnIdentityPool;
     private userPoolClient: UserPoolClient;
+
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
         this.createUserPool();
         this.createUserPoolClient();
+        this.createAdminsGroup();
+        this.createIdentityPool();
     }
     private createUserPool() {
         this.userPool = new UserPool(this, 'SpaceUserPool', {
@@ -42,6 +46,21 @@ export class AuthStack extends Stack {
         /**authFlows: the app client’s allowed authentication flows; these become the AuthFlow values you can pass to InitiateAuth/AdminInitiateAuth. */
         new CfnOutput(this, 'SpaceUserPoolClientId', {
             value: this.userPoolClient.userPoolClientId
+        });
+    }
+    private createAdminsGroup() {
+        new CfnUserPoolGroup(this, 'SpaceAdmins', { userPoolId: this.userPool.userPoolId, groupName: 'admins' })
+    }
+    private createIdentityPool() {
+        this.identityPool = new CfnIdentityPool(this, 'SpaceIdentityPool', {
+            allowUnauthenticatedIdentities: true,
+            cognitoIdentityProviders: [{
+                clientId: this.userPoolClient.userPoolClientId,
+                providerName: this.userPool.userPoolProviderName
+            }]
+        });
+        new CfnOutput(this, 'SpaceIdentityPoolId', {
+            value: this.identityPool.ref
         });
     }
 }
--- aws-cognito/src/services/hello.ts
+++ aws-cognito/src/services/hello.ts
@@ -1,12 +1,14 @@
 import { type APIGatewayProxyEvent, type APIGatewayProxyResult, type Context } from "aws-lambda";
 import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+import { hasAdminGroup } from "../../utils/utilMethods";
 
 const s3Client = new S3Client({})
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
     const listBucketCmd = new ListBucketsCommand({});
     const listBucketResult = (await s3Client.send(listBucketCmd)).Buckets;
-
+    const isAdminGroup = hasAdminGroup(event);
+    console.log("Has admin group",isAdminGroup);
     const response: APIGatewayProxyResult = {
         statusCode: 200,
         body: JSON.stringify("Hello from lambda, this is your buckets" + JSON.stringify(listBucketResult))
