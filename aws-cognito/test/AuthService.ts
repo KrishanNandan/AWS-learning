d834b1f12af0b9246db40fc3243163eb2facab3b
krishan nandan sharma <krishannandan09@gmail.com>
2025-09-23 03:24:44 +0530

    changes for identity pool

--- aws-cognito/test/AuthService.ts
+++ aws-cognito/test/AuthService.ts
@@ -1,12 +1,16 @@
 import { Amplify } from "aws-amplify";
 import { SignInOutput, fetchAuthSession, signIn } from "@aws-amplify/auth";
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
 
+const awsRegion = 'ap-south-1';
 
 Amplify.configure({
     Auth: {
         Cognito: {
             userPoolId: 'ap-south-1_RnArfbX1j',
-            userPoolClientId: '44m4jbeahne3na5i8gmk71mv63'
+            userPoolClientId: '44m4jbeahne3na5i8gmk71mv63',
+            identityPoolId: 'ap-south-1:14e61871-e334-44b1-b48d-561794f228cd'
         }
     }
 });
@@ -22,10 +26,25 @@ export class AuthService {
         })
         return SignInOutput;
     }
-
     /** Call only after login*/
     public async getIdToken() {
         const authSession = await fetchAuthSession();
         return authSession.tokens?.idToken?.toString();
+    }
+
+    public async generateTemporaryCredentials() {
+        const idToken = await this.getIdToken();
+        const cognitoIdentityPool = `cognito-idp.${awsRegion}.amazonaws.com/ap-south-1_RnArfbX1j`
+        const cognitoIdentity = new CognitoIdentityClient({
+            credentials: fromCognitoIdentityPool({
+                identityPoolId: 'ap-south-1:14e61871-e334-44b1-b48d-561794f228cd',
+                logins: {
+                    [cognitoIdentityPool]: idToken
+                }
+            })
+        });
+        const credentials = await cognitoIdentity.config.credentials();
+        console.log(credentials);
+        return credentials;
     }
 }
--- aws-cognito/test/auth.test.ts
+++ aws-cognito/test/auth.test.ts
@@ -1,10 +1,23 @@
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
 import { AuthService } from "./AuthService";
 
 async function testAuth() {
     const service = new AuthService();
-    const loginResult = await service.login("jyotikumari", "Bitu@Kumar@2710");
-    const token = await service.getIdToken();
-    console.log("krishan", loginResult, token);
+    await service.login("user", "password");
+    await service.getIdToken();
+    const credentials = await service.generateTemporaryCredentials();
+    const buckets = await listBuckets(credentials);
+    console.log("krishan", buckets);
+    const a = 5;
+}
+
+async function listBuckets(credentials: any) {
+    const client = new S3Client({
+        credentials: credentials
+    });
+    const command = new ListBucketsCommand({});
+    const result = await client.send(command);
+    return result;
 }
 
 testAuth();
